0603c39ba9d322e5abdcb6e9ead2ee392492affc
okhsunrog <me@okhsunrog.dev>
2025-05-30 10:03:23 +0300

    adding async executor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
+use esp_idf_svc::hal::task::thread::ThreadSpawnConfiguration;
+use embassy_executor::Executor;
+use embassy_time::Timer;
+use static_cell::StaticCell;
+use log::info;
+
+#[embassy_executor::task]
+async fn async_task() {
+    loop {
+        info!("Tick from embassy executor!");
+        Timer::after_secs(1).await;
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
 fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::timer::embassy_time_driver::link();
     esp_idf_svc::log::EspLogger::initialize_default();
+    let _eventfd_fs = esp_idf_svc::io::vfs::MountedEventfs::mount(5)?;
 
-    log::info!("Hello, world!");
+    log::info!("Starting high-prio executor");
+
+    ThreadSpawnConfiguration {
+        name: Some(b"async-exec-high\0"),
+        priority: 7,
+        ..Default::default()
+    }
+    .set()
+    .unwrap();
+
+    std::thread::Builder::new()
+        .stack_size(10_000)
+        .spawn(|| {
+            let executor = EXECUTOR.init(Executor::new());
+            executor.run(|spawner| {
+                spawner.spawn(async_task()).unwrap();
+            });
+        })?;
+
     Ok(())
 }
